223e1d8f9e75250da20f544da1b623a470ba769d
Arkvoodle7 <alexlego100@outlook.com>
2024-10-01 10:21:53 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ComprasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto1IngSoftware.Controllers
+{
+    public class ComprasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/VentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto1IngSoftware.Controllers
+{
+    public class VentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
