3f04fa547b7d34395185f33c7ce07f60e0b589bd
EstebanMZencode <esteban.malavasi.zuniga@cuc.cr>
2024-11-11 14:24:07 -0600

    OC y Proveedores

---
+++ APIs/Controllers/ProveedorController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIs.Models;
+using APIs.Services;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProveedorController : ControllerBase
+    {
+        private readonly IProveedorService _provService;
+        public ProveedorController(IProveedorService provService)
+        {
+            _provService = provService;
+        }
+
+        [HttpGet("proveedores")]
+        public async Task<ActionResult<List<Proveedor>>> ObtenerProveedores()
+        {
+            var proveedores = await _provService.ObtenerProveedoresAsync();
+            return Ok(proveedores);
+        }
+
+        [HttpGet("proveedor")]
+        public async Task<ActionResult<List<Proveedor>>> ObtenerProveedor(int id)
+        {
+            var proveedor = await _provService.ObtenerProveedorAsync(id);
+            return Ok(proveedor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Proveedor>> AgregarProv(Proveedor prov)
+        {
+            try
+            {
+                var nuevo_prov = await _provService.AgregarProvAsync(prov);
+                return Ok("Proveedor registardo con éxito");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Proveedor>> ActualizarProv(Proveedor prov)
+        {
+            try
+            {
+                var proveedor = await _provService.ActualizarProvAsync(prov);
+                return Ok("Proveedor actualizado correctamente");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Proveedor>> EliminarProv(int id)
+        {
+            try
+            {
+                var proveedor = await _provService.EliminarProvAsync(id);
+                return Ok("Proveedor eliminado exitosamente");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ APIs/Services/IProdXOCService.cs
@@ -0,0 +1,12 @@
+﻿using APIs.Models;
+
+namespace APIs.Services
+{
+    public interface IProdXOCService
+    {
+        Task<ProdXOC> AgregarProdXOCAsync(ProdXOC prodXoc);
+        Task<ProdXOC> EliminarProdXOCAsync(ProdXOC prodXoc);
+        Task<List<ProdXOC>> ObtenerProdsXOCAsync(int oc);
+    }
+}
+
